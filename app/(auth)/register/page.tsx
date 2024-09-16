d18e92ba2d5ed1441027403c3062c2d640fc3c28
Mustafa Bugra Serce <serce_bugra@hotmail.com>
2024-09-16 15:22:02 +0300

    first commit

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+// pages/sign-up.js or pages/sign-up.tsx
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>Register
+      
+    </div>
+  );
+}
+
+export default Register;
